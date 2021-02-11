dcba480b4eae0738cbdeb99f9fa872c24b78979d
Mona Kulmala <mlkulmala@hotmail.com>
2021-02-11 11:31:10 +0200

    Changes in templates

--- routes.py
+++ routes.py
@@ -5,3 +5,15 @@ from flask import render_template
 @app.route("/")
 def index():
     return render_template("index.html") 
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    session["username"] = username
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
