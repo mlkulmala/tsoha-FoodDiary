ce1db5ddd4266f0852bd2b0ee2a6052793cbfcd4
Mona Kulmala <mlkulmala@hotmail.com>
2021-02-15 17:06:11 +0200

    update

--- foodstuffs.py
+++ foodstuffs.py
@@ -1,3 +1,31 @@
 from db import db
 
 
+def get_foodstuffs():
+    sql = "SELECT * FROM foodstuffs"
+    result = db.session.execute(sql)
+    return result.fetchall()
+
+def get_foodstuff_by_name(food_name):
+    sql = "SELECT name FROM foodstuffs WHERE name LIKE '%food_name%'"
+    result = db.session.execute(sql)
+    return result.fetchall()
+
+def dictionary(foodstuff):
+    dictionary = {
+	    "id": foodstuff[0],
+	    "name": foodstuff[1],
+	    "calories": foodstuff[2],
+	    "fat": foodstuff[3],
+	    "carbs": foodstuff[4],
+	    "protein": foodstuff[5],
+	    "fiber": foodstuff[6]
+        }
+    return dictionary
+
+def foodstuffs_to_dictionary(foodstuffs):
+    foodstuffs = []
+    for foodstuff in foodstuffs:
+        foodstuffs.append(dictionary(foodstuff))
+    return foodstuffs
+
--- routes.py
+++ routes.py
@@ -1,19 +1,45 @@
 from app import app
-import foodstuffs
-from flask import render_template
+from flask import redirect, render_template, request
+import users, foodstuffs
 
 @app.route("/")
 def index():
-    return render_template("index.html") 
+    if request.args:
+        meal = request.args["meal"]
+        foodstuff = request.args["foodstuff"]
+        amount = request.args["amount"]
+    else:
+        selected_foodstuffs = []
+    selected_foodstuffs = []
+    selected_foodstuffs = foodstuffs.get_foodstuffs()
+    return render_template("index.html", foodstuffs=selected_foodstuffs) 
 
-@app.route("/login", methods=["POST"])
+@app.route("/login", methods=["GET","POST"])
 def login():
-    username = request.form["username"]
-    password = request.form["password"]
-    session["username"] = username
-    return redirect("/")
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.login(username, password):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Väärä tunnus tai salasana")
 
 @app.route("/logout")
 def logout():
-    del session["username"]
+    users.logout()
     return redirect("/")
+
+@app.route("/register", methods=["GET","POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.register(username, password):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Tunnuksen luominen ei onnistunut")
+
