901cfafd1f04d5ac633fad568b71209a51860733
Mona Kulmala <mlkulmala@hotmail.com>
2021-02-21 18:21:02 +0200

    problems with request.form <- solve this

--- routes.py
+++ routes.py
@@ -1,35 +1,27 @@
 from app import app 
 from flask import redirect, render_template, request 
-import users, foodstuffs, fooddiaries
+import users, foodstuffs, fooddiaries, utils
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def index():
     user_id = users.user_id()
     portions = []
-    foodstuff = None
-    meal = None
-    amount = None
-    if request.args:
-        foodstuff = request.args["foodstuff"]
-        meal = request.args["meal"]
-        amount = request.args["amount"]
-        if user_id != 0:
-            portions = fooddiaries.add_to_diary(user_id, foodstuff, meal, amount)
-    return render_template("index.html", foodstuff=foodstuff, meal=meal, amount=amount, portions=portions)
+    if user_id != 0:
+        portions = fooddiaries.get_todays_diary(user_id)
+        if request.args:
+            foodstuff_id = request.args["foodstuff"]
+            meal_id = utils.get_meal_id(request.args["meal"])
+            amount = request.args["amount"]
+            fooddiaries.add_to_diary(user_id, foodstuff_id, meal_id, amount)
+    return render_template("index.html", portions=portions)
 
 
 @app.route("/select_food", methods=["GET"])
 def select_food():
     food_name = request.args["searched_food"]
     foodlist = foodstuffs.get_foodstuff_by_name(food_name)
-    return render_template("select_food.html", food_name=food_name, foodstuffs=foodlist)
-
-@app.route("/add_portion", methods=["GET"])
-def add_portion():
-    user_id = users.user_id()
-    if user_id != 0:
-        fooddiaries.add_to_diary(user_id, date, foodstuff, meal, amount)
-    return redirect("/")
+    meals = utils.get_meals()
+    return render_template("select_food.html", food_name=food_name, foodstuffs=foodlist, meals=meals)
 
 
 
