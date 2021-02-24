4422f921a088c80c297db3878aa27f7eb46305e5
Mona Kulmala <mlkulmala@hotmail.com>
2021-02-24 19:04:45 +0200

    updated sql queries

--- utils.py
+++ utils.py
@@ -11,3 +11,16 @@ def get_meal_id(meal_name):
     meal_id = result.fetchone()[0]
     return meal_id
 
+def add_personal_details(user_id, gender_id, age, height, weight, activity):
+    sql = "INSERT INTO personal_details (user_id, gender_id, age, height, weight, activity) " \
+        "VALUES (:user_id, :gender_id, :age, :height, :weight, :activity)"
+    result = db.session.execute(sql, {"user_id":user_id, "gender_id":gender_id, "age":age, \
+        "height":height, "weight":weight, "activity":activity})
+    db.session.commit()
+    return True
+
+def get_gender(gender_id):
+    sql = "SELECT gender FROM gender WHERE id=:gender_id"
+    result = db.session.execute(sql, {"gender_id":gender_id})
+    gender = result.fetchone()[0]
+    return gender
