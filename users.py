132fa3b93cb06ab29eba27b19430b6f6f6f53820
Mona Kulmala <mlkulmala@hotmail.com>
2021-02-24 19:04:05 +0200

    new sql queries for personal details

--- users.py
+++ users.py
@@ -32,4 +32,25 @@ def register(username,password):
 
 def user_id():
     return session.get("user_id",0)
-    
+
+def personal_details(user_id):
+    sql = "SELECT gender, age, height, weight, activity FROM personal_details " \
+        "JOIN users ON users.id = personal_details.user_id " \
+        "JOIN gender ON gender.id = personal_details.gender_id " \
+        "WHERE users.id=:user_id"
+    result = db.session.execute(sql, {"user_id":user_id})
+    return result.fetchone()
+
+def add_personal_details(user_id, gender_id, age, height, weight, activity):
+    sql = "INSERT INTO personal_details (user_id, gender_id, age, height, weight, activity) " \
+        "VALUES (:user_id, :gender_id, :age, :height, :weight, :activity)"
+    result = db.session.execute(sql, {"user_id":user_id, "gender_id":gender_id, "age":age, \
+        "height":height, "weight":weight, "activity":activity})
+    db.session.commit()
+    return True
+
+def get_gender(gender_id):
+    sql = "SELECT gender FROM gender WHERE id=:gender_id"
+    result = db.session.execute(sql, {"gender_id":gender_id})
+    gender = result.fetchone()[0]
+    return gender
