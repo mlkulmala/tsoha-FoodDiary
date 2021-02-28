81e632fef24824b2719e820fb9fb97e7d2e16858
Mona Kulmala <mlkulmala@hotmail.com>
2021-02-28 23:08:40 +0200

    added functions for counting personal calorie goal

--- users.py
+++ users.py
@@ -1,5 +1,6 @@
 from db import db
 from flask import session
+import re
 from werkzeug.security import check_password_hash, generate_password_hash
 
 def login(username,password):
@@ -33,6 +34,13 @@ def register(username,password):
 def user_id():
     return session.get("user_id",0)
 
+def password_qualified(password):
+    if len(password) < 8:
+        return False
+    if re.search("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$", password):
+        return True
+    return False
+
 def personal_details(user_id):
     sql = "SELECT gender, age, height, weight, activity FROM personal_details " \
         "JOIN users ON users.id = personal_details.user_id " \
@@ -40,6 +48,14 @@ def personal_details(user_id):
         "WHERE users.id=:user_id"
     result = db.session.execute(sql, {"user_id":user_id})
     return result.fetchone()
+
+def has_profile(user_id):
+    sql = "SELECT id FROM personal_details WHERE user_id=:user_id"
+    result = db.session.execute(sql, {"user_id":user_id})
+    if (result.fetchone() == None):
+        return False
+    return True
+
 
 def add_personal_details(user_id, gender_id, age, height, weight, activity):
     sql = "INSERT INTO personal_details (user_id, gender_id, age, height, weight, activity) " \
@@ -49,8 +65,43 @@ def add_personal_details(user_id, gender_id, age, height, weight, activity):
     db.session.commit()
     return True
 
+def update_personal_details(user_id, gender_id, age, height, weight, activity):
+    sql = "UPDATE personal_details SET gender_id=:gender_id, age=:age, height=:height, weight=:weight, " \
+        "activity=:activity WHERE user_id=:user_id"
+    result = db.session.execute(sql, {"user_id":user_id, "gender_id":gender_id, "age":age, \
+        "height":height, "weight":weight, "activity":activity})
+    db.session.commit()
+    return True
+
+
 def get_gender(gender_id):
     sql = "SELECT gender FROM gender WHERE id=:gender_id"
     result = db.session.execute(sql, {"gender_id":gender_id})
     gender = result.fetchone()[0]
     return gender
+
+def add_calorie_goal(user_id, gender_id, age, height, weight, activity):
+    if gender_id == 1:
+        calorie_goal = round(float(activity) * (447.593 + (9.247 * float(weight)) + \
+            (3.098 * float(height)) - (4.33 * float(age))))
+    else:
+        calorie_goal = round(float(activity) * (88.362 + (13.397 * float(weight)) + \
+            (4.799 * float(height)) - (5.677 * float(age))))
+    sql = "UPDATE personal_details SET calorie_goal=:calorie_goal WHERE user_id=:user_id"
+    result = db.session.execute(sql, {"calorie_goal":calorie_goal, "user_id":user_id})
+    db.session.commit()
+    return True
+
+def get_calorie_goal(user_id):
+    sql = "SELECT calorie_goal FROM personal_details WHERE user_id=:user_id"
+    result = db.session.execute(sql, {"user_id":user_id})
+    calorie_goal = result.fetchone()[0]
+    if (calorie_goal == None):
+        return 2000
+    return calorie_goal
+
+def set_calorie_goal(user_id, goal):
+    sql = "UPDATE personal_details SET calorie_goal=:calorie_goal WHERE user_id=:user_id"
+    result = db.session.execute(sql, {"calorie_goal":goal})
+    db.session.commit()
+    return True
