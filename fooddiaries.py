1c19a0b7c1c59354c780a6a4515650689c2bd15c
Mona Kulmala <mlkulmala@hotmail.com>
2021-02-28 23:07:26 +0200

    added get_calories_and_nutrients()

--- fooddiaries.py
+++ fooddiaries.py
@@ -2,15 +2,25 @@ from db import db
 
 
 def add_to_diary(user_id, foodstuff_id, meal_id, amount):
-    sql_diary = "INSERT INTO food_diaries (user_id) VALUES (:user_id) RETURNING id"
-    result = db.session.execute(sql_diary, {"user_id":user_id})
-    db.session.commit()
-    diary_id = result.fetchone()[0]
+    sql = "SELECT id FROM food_diaries WHERE user_id=:user_id AND date = current_date"
+    result = db.session.execute(sql, {"user_id":user_id})
+    row = result.fetchone()
+    if row == None:
+        diary_id = create_new_diary(user_id)
+    else:
+        diary_id = row[0]
     sql_portions = "INSERT INTO portions (diary_id, foodstuff_id, meal_id, amount)" \
         "VALUES (:diary_id, :foodstuff_id, :meal_id, :amount)"
     db.session.execute(sql_portions, {"diary_id":diary_id, "foodstuff_id":foodstuff_id, "meal_id":meal_id, "amount":amount})
     db.session.commit()
     return True
+
+def create_new_diary(user_id):
+    sql = "INSERT INTO food_diaries (user_id) VALUES (:user_id) RETURNING id"
+    result = db.session.execute(sql, {"user_id":user_id})
+    db.session.commit()
+    return result.fetchone()[0]
+
 
 def get_todays_diary(user_id):
     sql = "SELECT meals.id, meals.name, foodstuffs.name, amount, calories, " \
@@ -23,10 +33,41 @@ def get_todays_diary(user_id):
     result = db.session.execute(sql, {"user_id":user_id})
     return result.fetchall()
 
-def get_total_calories(user_id):
-    sql = "SELECT SUM(calories) FROM foodstuffs " \
+def get_diary_by_date(user_id, date):
+    sql = "SELECT meals.id, meals.name, foodstuffs.name, amount, "\
+        "calories, fat, carbs, protein, fiber FROM food_diaries " \
+        "INNER JOIN portions ON (food_diaries.id = portions.diary_id) " \
+        "INNER JOIN foodstuffs ON (portions.foodstuff_id = foodstuffs.id) " \
+        "INNER JOIN meals ON (meals.id = portions.meal_id) " \
+        "WHERE user_id=:user_id AND date=:date " \
+        "ORDER BY date DESC, meals.id, meals.name"
+    result = db.session.execute(sql, {"user_id":user_id, "date":date})
+    return result.fetchall()
+
+
+def get_calories_and_nutrients(user_id, date):
+    sql = "SELECT portions.amount, calories, fat, carbs, protein, fiber FROM foodstuffs " \
         "INNER JOIN portions ON (portions.foodstuff_id = foodstuffs.id) " \
         "INNER JOIN food_diaries ON (food_diaries.id = portions.diary_id) " \
-        "WHERE user_id=:user_id AND date = current_date"
-    result = db.session.execute(sql, {"user_id":user_id})
-    return result.fetchone()[0]
+        "WHERE user_id=:user_id AND date=:date"
+    result = db.session.execute(sql, {"user_id":user_id, "date":date})
+    rows = result.fetchall()
+    if len(rows) == 0:
+        return [0, 0, 0, 0, 0]
+    else:
+        # count total amount of each nutrient per each diary entry
+        total_kcal, total_fat, total_carbs, total_protein, total_fiber = 0, 0, 0, 0, 0
+        for row in rows:
+            total_kcal += row[0] / 100 * row[1]
+            total_fat += row[0] / 100 * float(row[2])
+            total_carbs += row[0] / 100 * float(row[3])
+            total_protein += row[0] / 100 * float(row[4])
+            total_fiber += row[0] / 100 * float(row[5])
+        # count the percentage of energy received from each nutrient
+        kcal = round(total_kcal)
+        fat = round(total_fat * 900 / total_kcal, 1)
+        carbs = round(total_carbs * 400 / total_kcal, 1)
+        pro = round(total_protein * 400 / total_kcal, 1)
+        fiber = round(total_fiber)
+        values = [kcal, fat, carbs, pro, fiber]
+        return values
