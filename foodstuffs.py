47b849b6ce5bfc299c651ec21893d72fd810174c
Mona Kulmala <mlkulmala@hotmail.com>
2021-03-03 17:34:11 +0200

    updated sql queries

--- foodstuffs.py
+++ foodstuffs.py
@@ -1,31 +1,18 @@
 from db import db
 
 
-def get_foodstuffs():
-    sql = "SELECT * FROM foodstuffs"
-    result = db.session.execute(sql)
+
+def get_foodstuff_by_name(food_name, user_id):
+    sql = "SELECT * FROM foodstuffs WHERE name ILIKE :name AND (added_by=:user_id OR visible=true)"
+    result = db.session.execute(sql, {"name":'%%'+food_name+'%%', "user_id":user_id})
     return result.fetchall()
 
-def get_foodstuff_by_name(food_name):
-    sql = "SELECT * FROM foodstuffs WHERE name ILIKE :name"
-    result = db.session.execute(sql, {"name":'%%'+food_name+'%%'})
-    return result.fetchall()
+def add_foodstuff(name, kcal, fat, carbs, pro, fiber, user_id):
+    sql = "INSERT INTO foodstuffs (name, calories, fat, carbs, protein, fiber, added_by, visible) " \
+        "VALUES (:name, :calories, :fat, :carbs, :protein, :fiber, :added_by, false)"
+    db.session.execute(sql, {"name":name, "calories":kcal, "fat":fat, "carbs":carbs, "protein":pro, \
+        "fiber":fiber, "added_by":user_id})
+    db.session.commit()
+    return True
 
-def dictionary(foodstuff):
-    dictionary = {
-	    "id": foodstuff[0],
-	    "name": foodstuff[1],
-	    "calories": foodstuff[2],
-	    "fat": foodstuff[3],
-	    "carbs": foodstuff[4],
-	    "protein": foodstuff[5],
-	    "fiber": foodstuff[6]
-        }
-    return dictionary
-
-def foodstuffs_to_dictionary(foodstuffs):
-    foodstuffs = []
-    for foodstuff in foodstuffs:
-        foodstuffs.append(dictionary(foodstuff))
-    return foodstuffs
 
