2e2b85de9f0f835b90fbc6885574697226c1b5e6
Mona Kulmala <mlkulmala@hotmail.com>
2021-03-03 17:36:38 +0200

    moved some functions to foodstuffs.py

--- utils.py
+++ utils.py
@@ -11,23 +11,9 @@ def get_meal_id(meal_name):
     meal_id = result.fetchone()[0]
     return meal_id
 
-def add_personal_details(user_id, gender_id, age, height, weight, activity):
-    sql = "INSERT INTO personal_details (user_id, gender_id, age, height, weight, activity) " \
-        "VALUES (:user_id, :gender_id, :age, :height, :weight, :activity)"
-    result = db.session.execute(sql, {"user_id":user_id, "gender_id":gender_id, "age":age, \
-        "height":height, "weight":weight, "activity":activity})
-    db.session.commit()
-    return True
-
 def get_gender(gender_id):
     sql = "SELECT gender FROM gender WHERE id=:gender_id"
     result = db.session.execute(sql, {"gender_id":gender_id})
     gender = result.fetchone()[0]
     return gender
 
-def count_energy_consumption(gender_id, age, height, weight, activity):
-    if gender_id == 1:
-        consumption = activity * (447.593 + (9.247 * weight) + (3.098 * height) - (4.33 * age))
-    else:
-        consumption = activity * (88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age))
-    return consumption
