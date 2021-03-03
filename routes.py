83f8e8620d04285484a2edbff5cce58096e8fc7b
Mona Kulmala <mlkulmala@hotmail.com>
2021-03-03 17:34:44 +0200

    added new functions

--- routes.py
+++ routes.py
@@ -1,54 +1,18 @@
 from app import app
 from flask import redirect, render_template, request, session
 from datetime import datetime
-import users, foodstuffs, fooddiaries, utils
+import foodstuffs, fooddiaries, persons, users, utils
+
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/food_search")
-def search():
-    user_id = users.user_id()
-    portions = fooddiaries.get_todays_diary(user_id)
-    current_date = datetime.today().strftime('%Y-%m-%d')
-    total = fooddiaries.get_calories_and_nutrients(user_id, current_date)
-    kcal = total[0]
-    fat = total[1]
-    carbs = total[2]
-    pro = total[3]
-    fiber = total[4]
-    calorie_goal = users.get_calorie_goal(user_id)
-    kcal_left = calorie_goal - kcal
-    return render_template("food_search.html", portions=portions, calorie_goal=calorie_goal, \
-         kcal=kcal, fat=fat, carbs=carbs, pro=pro, fiber=fiber, kcal_left=kcal_left)
-
-
-@app.route("/select_food", methods=["GET"])
-def select_food():
-    food_name = request.args["searched_food"]
-    food_list = foodstuffs.get_foodstuff_by_name(food_name)
-    meals = utils.get_meals()
-    return render_template("select_food.html", food_name=food_name, foodstuffs=food_list, meals=meals)
-
-@app.route("/diary_entry", methods=["POST"])
-def diary_entry():
-    user_id = users.user_id()
-    if request.form:
-        foodstuff_id = request.form["foodstuff"]
-        meal_id = utils.get_meal_id(request.form["meal"])
-        amount = request.form["amount"]
-        if fooddiaries.add_to_diary(user_id, foodstuff_id, meal_id, amount):
-            return redirect("/food_search")
-        else:
-            return render_template("error.html", message="Lisäys ruokapäiväkirjaan ei onnistunut.")
-    return render_template("error.html", message="Tarkista, että teit kaikki valinnat.")
-
-
 @app.route("/login", methods=["GET","POST"])
 def login():
     if request.method == "GET":
-        return render_template("login.html")
+        return render_template("login.html", error=False)
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
@@ -56,7 +20,7 @@ def login():
         if users.login(username, password):
             return redirect("/food_search")
         else:
-            return render_template("error.html", message="Väärä tunnus tai salasana")
+            return render_template("login.html", error=True, message="Väärä tunnus tai salasana")
 
 @app.route("/logout")
 def logout():
@@ -68,6 +32,8 @@ def register():
     if request.method == "GET":
         return render_template("register.html")
     if request.method == "POST":
+        if (session["csrf_token"] != request.form["csrf_token"]):
+            return abort(403)
         username = request.form["username"]
         password = request.form["password"]
         password_val = request.form["validate_password"]
@@ -82,6 +48,47 @@ def register():
         else:
             return render_template("register.html", error=True, message="Tunnuksen luominen ei onnistunut.")
 
+@app.route("/food_search")
+def search():
+    user_id = users.user_id()
+    portions = fooddiaries.get_todays_diary(user_id)
+    current_date = datetime.today().strftime('%Y-%m-%d')
+    total = fooddiaries.get_calories_and_nutrients(user_id, current_date)
+    kcal = total[0]
+    fat = total[1]
+    carbs = total[2]
+    pro = total[3]
+    fiber = total[4]
+    calorie_goal = persons.get_calorie_goal(user_id)
+    kcal_left = calorie_goal - kcal
+    return render_template("food_search.html", portions=portions, calorie_goal=calorie_goal, \
+         kcal=kcal, fat=fat, carbs=carbs, pro=pro, fiber=fiber, kcal_left=kcal_left)
+
+
+@app.route("/select_food", methods=["GET"])
+def select_food():
+    user_id = users.user_id()
+    food_name = request.args["searched_food"]
+    food_list = foodstuffs.get_foodstuff_by_name(food_name, user_id)
+    meals = utils.get_meals()
+    return render_template("select_food.html", food_name=food_name, foodstuffs=food_list, meals=meals)
+
+@app.route("/diary_entry", methods=["POST"])
+def diary_entry():
+    user_id = users.user_id()
+    if (session["csrf_token"] != request.form["csrf_token"]):
+        return abort(403)
+    if request.form["foodstuff"]:
+        foodstuff_id = request.form["foodstuff"]
+        meal_id = utils.get_meal_id(request.form["meal"])
+        amount = request.form["amount"]
+        if fooddiaries.add_to_diary(user_id, foodstuff_id, meal_id, amount):
+            return redirect("/food_search")
+        else:
+            return render_template("error.html", message="Lisäys ruokapäiväkirjaan ei onnistunut.")
+    return render_template("error.html", message="Tarkista, että teit kaikki valinnat.")
+
+
 @app.route("/create_profile")
 def create_profile():
     return render_template("personal_details.html")
@@ -90,17 +97,19 @@ def create_profile():
 def add_details():
     user_id = users.user_id()
     if request.form:
+        if (session["csrf_token"] != request.form["csrf_token"]):
+            return abort(403)
         gender_id = request.form["gender"]
-        gender = users.get_gender(gender_id)
+        gender = persons.get_gender(gender_id)
         age = request.form["age"]
         height = request.form["height"]
         weight = request.form["weight"]
         activity = request.form["activity"]
-        users.add_calorie_goal(user_id, gender_id, age, height, weight, activity)
+        persons.add_calorie_goal(user_id, gender_id, age, height, weight, activity)
         if users.has_profile(user_id):
-            if users.update_personal_details(user_id, gender_id, age, height, weight, activity):
+            if persons.update_personal_details(user_id, gender_id, age, height, weight, activity):
                 return redirect("/profile/"+str(user_id))
-        if users.add_personal_details(user_id, gender_id, age, height, weight, activity):
+        if persons.add_personal_details(user_id, gender_id, age, height, weight, activity):
             return redirect("/profile/"+str(user_id))
         else:
             return render_template("error.html", message="Tietojen lisääminen ei onnistunut")
@@ -108,8 +117,8 @@ def add_details():
 
 @app.route("/profile/<int:id>")
 def profile(id):
-    details = users.personal_details(id)
-    calorie_goal = users.get_calorie_goal(id)
+    details = persons.get_personal_details(id)
+    calorie_goal = persons.get_calorie_goal(id)
     return render_template("profile.html", details=details, calorie_goal=calorie_goal)
 
 @app.route("/diary_days", methods=["GET"])
@@ -119,14 +128,13 @@ def diary_days():
         date = request.args["diary_date"]
     else:
         date = datetime.today().strftime('%Y-%m-%d')
-    # date = datetime.today().strftime('%Y-%m-%d')
     total = fooddiaries.get_calories_and_nutrients(user_id, date)
     kcal = total[0]
     fat = total[1]
     carbs = total[2]
     pro = total[3]
     fiber = total[4]
-    calorie_goal = users.get_calorie_goal(user_id)
+    calorie_goal = persons.get_calorie_goal(user_id)
     portions = fooddiaries.get_diary_by_date(user_id, date)
     return render_template("diary_days.html", portions=portions, calorie_goal=calorie_goal, \
          kcal=kcal, fat=fat, carbs=carbs, pro=pro, fiber=fiber)
@@ -141,8 +149,44 @@ def get_diaries():
     carbs = total[2]
     pro = total[3]
     fiber = total[4]
-    calorie_goal = users.get_calorie_goal(user_id)
+    calorie_goal = persons.get_calorie_goal(user_id)
     portions = fooddiaries.get_diary_by_date(user_id, date)
     return render_template("diary_days.html", portions=portions, calorie_goal=calorie_goal, \
          kcal=kcal, fat=fat, carbs=carbs, pro=pro, fiber=fiber)
 
+@app.route("/remove_from_diary", methods=["GET"])
+def remove_from_diary():
+    portion_id = request.args["remove"]
+    print(portion_id)
+    selected = fooddiaries.get_portion(portion_id)
+    meal= selected[0]
+    name = selected[1]
+    amount = selected[2]
+    return render_template("remove.html", meal=meal, name=name, amount=amount, portion_id=portion_id)
+
+@app.route("/delete", methods=["GET"])
+def delete():
+    portion_id = request.args["portion_id"]
+    portion_deleted = fooddiaries.delete_portion(portion_id)
+    if portion_deleted:
+        return redirect("/food_search")
+    else:
+        return render_template("error.html", message="Päiväkirjamerkinnän poisto ei onnistunut.")
+
+@app.route("/new_foodstuff")
+def new_foodstuff():
+    return render_template("new_foodstuff.html")
+
+@app.route("/add_foodstuff", methods=["POST"])
+def add_foodstuff():
+    user_id = users.user_id()
+    name = request.form["name"]
+    kcal = request.form["kcal"]
+    fat = request.form["fat"]
+    carbs = request.form["fat"]
+    pro = request.form["pro"]
+    fiber = request.form["fiber"]
+    if foodstuffs.add_foodstuff(name, kcal, fat, carbs, pro, fiber, user_id):
+        return render_template("foodstuff_added.html", name=name, error=False)
+    else:
+        return render_template("foodstuff_added.html", name=name, error=True)
